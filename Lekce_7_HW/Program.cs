011fc26956716d8a6167074c4e640fc2c5123776
Petr <bojko.petr2006@gmail.com>
2024-12-06 20:45:21 +0100

    Pridan git

---
+++ Lekce_7_HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekce_7_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
